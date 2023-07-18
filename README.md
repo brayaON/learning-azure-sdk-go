ba1d290c5b6c504163301dd58fd1fb69a535ee77
brayaON <bortizf@unal.edu.co>
2023-07-18 18:39:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AZURE SDK FOR LEARNING PROCESS
+
+This repository contains a collection of programs I've developed to practice Go whilst learning Azure Cloud.
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+    "github.com/brayaON/go-sdk-programs/account"
 )
 
 func main() {
+    //account.UploadBlobs()
+    //account.ListBlobs()
+    account.DownloadBlobs()
 }
